eba36de8d7f8ae71de44d6d2031e4312a4a9718a
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-03-05 19:18:05 +0200

    Greeting state (task resolve)

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -6,17 +6,26 @@ class Greeting extends React.Component {
 
         this.state = {
             // 1. Створити змінну стейту (за аналогією з лампочкою)
+            isGreeting: true
         }
     }
 
     // 2. Зробити обробник події кліку на h1 (за аналогією з лампочкою)
+
+    clickHandler() {
+        this.setState({
+            isGreeting: !this.state.isGreeting
+        });
+    }
 
     // тут можуть бути якісь обробники подій
 
     render() {
         // 3. В залежності від значення стейту, вирахувати що за текст ви показуєте (за аналогією з лампочкою)
 
-        return <h1>Hello {this.props.name}</h1>
+        const greetingText = this.state.isGreeting ? 'Hello' : 'Bye';
+
+        return <h1 onClick={() => {this.clickHandler()}}>{greetingText} {this.props.name}</h1>
         // P.S. Не забудьте підписати h1 на обробник кліку, розроблений у п.2
     }
 }
