a8b7bb33a448d1d46f5d6e6ba8edefee99491fa1
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-03-07 20:06:47 +0200

    Add task

--- src/components/Scene/Toggler.js
+++ src/components/Scene/Toggler.js
@@ -1,16 +1,27 @@
 import React from "react";
 
-class Toggler extends React.Component {
-    clickHandler() {
-        this.props.changeParentState();
+// class Toggler extends React.Component {
+//     clickHandler() {
+//         this.props.changeParentState();
+//     }
+
+//     render() {
+//         return (
+//             <button onClick={() => {this.clickHandler()}}>{this.props.buttonText}</button>
+//         )
+//     }
+// }
+
+// export default Toggler;
+
+function Toggler (props) {
+    const clickHandler = () => {
+        props.changeParentState();
     }
 
-
-    render() {
-        return (
-            <button onClick={() => {this.clickHandler()}}>{this.props.buttonText}</button>
-        )
-    }
+    return (
+        <button onClick={() => {clickHandler()}}>{props.buttonText}</button>
+    )
 }
 
 export default Toggler;
