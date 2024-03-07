3f2dbd117eaed2cc06006055d18c80732652555a
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-03-07 19:56:02 +0200

    Refactor

--- src/components/Scene/Scene.js
+++ src/components/Scene/Scene.js
@@ -22,7 +22,7 @@ class Scene extends React.Component {
         const { isLight } = this.state;
         return (
             <div className={isLight ? "scene-on" : "scene-off"}>
-                <Toggler changeParentState={() => {this.toggleScene()}} />
+                <Toggler changeParentState={() => {this.toggleScene()}} buttonText={isLight ? 'Off' : 'On'} />
             </div>
         )
     }
--- src/components/Scene/Toggler.js
+++ src/components/Scene/Toggler.js
@@ -1,26 +1,14 @@
 import React from "react";
 
 class Toggler extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            isOn: true
-        }
-    }
-
     clickHandler() {
-        this.setState({
-            isOn: !this.state.isOn
-        });
-
         this.props.changeParentState();
     }
 
 
     render() {
         return (
-            <button onClick={() => {this.clickHandler()}}>{this.state.isOn ? 'Off' : 'On'}</button>
+            <button onClick={() => {this.clickHandler()}}>{this.props.buttonText}</button>
         )
     }
 }
