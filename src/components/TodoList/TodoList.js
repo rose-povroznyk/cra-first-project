b8c9755f777b4313c2164e483c8ede40051b3f6d
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-03-07 18:16:47 +0200

    TodoList task 1 resolve

---
+++ src/components/TodoList/TodoList.js
@@ -0,0 +1,68 @@
+import React from "react";
+
+class TodoList extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            todoList: [
+                {id: 1, text: 'go to party'},
+                {id: 2, text: 'meet friends'},
+                {id: 3, text: 'have a walk'}
+            ]
+        }
+    }
+
+    renderLi() {
+        const { todoList } = this.state;
+
+        return todoList.map((task) => <li key={task.id}>{task.text}</li>);
+    }
+
+    render() {
+        return (
+            <>
+                <h1>TODO LIST</h1>
+
+                <ul>
+                    {this.renderLi()}
+                </ul>
+            </>
+        )
+    }
+}
+
+export default TodoList;
+
+/*
+
+Задача 1 - DONE
+
++ Створити компоненту TodoList.
++ Ця компонента має в стейті масив об'єктів, в якому будуть знаходитись текст задачі та її id.
+
+Приблизно це може виглядати ось таким чином:
+
+[
+    {id: 1, text: 'go to party'},
+    {id: 2, text: 'meet friends'},
+    {id: 3, text: 'have a walk'}
+]
+
++ Компонента рендерить список <li>, всередині якого один рядок з масиву
+
+Приблизно це може виглядати ось таким чином:
+
+<ul>
+    <li>go to party</li>
+    <li>meet friends</li>
+    <li>have a walk</li>
+</ul>
+
+
+Задача 2 (***)
+
+В кожній li зробити кнопку, за натисненням на яку цей елемент списку має зникнути (реалізувати видалення завдання)
+(тобто оновити стейт таким чином, щоб в масиві з рядками або об'єктами не було того, який пов'язаний з кнопкою)
+
+*/
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AlohaDashboard from './components/AlohaDashboard/AlohaDashboard';
+import TodoList from './components/TodoList/TodoList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <AlohaDashboard />
+  <TodoList />
 );
